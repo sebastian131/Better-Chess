adc60cd366becba4cfd18c3404dc3f5d4ad2bad2
sebastian131 <swhofman@gmail.com>
2016-08-15 16:06:26 -0400

    Add files via upload

---
+++ Whitepawn.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pawn : MonoBehaviour {
+	public GameObject WhitePawn;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void CreateSphere (float _posX, float _posY, float _posZ)
+	{
+		GameObject.Instantiate (WhitePawn, new Vector3 (_posX, _posY, _posZ), Quaternion.identity);
+	}
+}
---
+++ makeBoard.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections; 
+public class makeBoard : MonoBehaviour {
+	private static GameObject[,]tiles = new GameObject[11, 11];
+public GameObject redCube;
+public GameObject greyCube;
+	// Use this for initialization
+	void Start () {
+		Debug.Log (tiles [1, 1]);
+		for (int i = 0; i < 121; i++) 
+		{
+			CreateCube(i/11, 0, i%11, i);
+			
+		}
+	}
+	void CreateCube(int _posX, int _posY, int _posZ, int _CColor)
+	{
+		if (_CColor % 2 == 0) {
+			GameObject.Instantiate (redCube, new Vector3 (_posX, _posY, _posZ), Quaternion.identity);
+		} 
+		else {
+			GameObject.Instantiate (greyCube, new Vector3 (_posX, _posY, _posZ), Quaternion.identity);
+		}
+		
+	} 
+	GameObject getTile (int x, int y)
+	{
+		return tiles[x,y]; 	
+	}
+	void setTile (int x, int y, GameObject piece)
+	{
+		tiles [x, y] = piece;
+	}
+}
